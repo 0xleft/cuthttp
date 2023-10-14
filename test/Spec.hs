2f34059db216eb9d230919a47993399cda5ee3d6
AdomasDauda <107749872+0xleft@users.noreply.github.com>
2023-10-14 16:32:54 +0100

    update licensing

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,6 +17,7 @@ main = hspec $ do
 
     describe "make socket" $ do
         it "should return valid socket" $ do
+            -- my server --
             let server = Server "194.135.85.93" 80
             let request = Request GET "/" "HTTP/1.1" [("Content-Type", "application/json")] "Hello" "Bad"
             recvUntil request server `shouldReturn` "HTTP/1.1 400 Bad"
