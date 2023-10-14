62ed91f06269f61e2f85091b8e456c451544f32f
AdomasDauda <107749872+0xleft@users.noreply.github.com>
2023-10-14 15:02:31 +0100

    working version

--- src/CutHttp.hs
+++ src/CutHttp.hs
@@ -1,10 +1,16 @@
 module CutHttp(
-    RequestType(..)
+    makeRequest
+    , recvUntil
+    , RequestType(..)
     , Request(..)
     , Server(..)
 ) where
 
 import Network.Socket
+import Network.Socket.ByteString (send, recv)
+import qualified Data.ByteString.Char8 as BS
+import Data.ByteString.Char8 (unpack)
+import Data.List (isInfixOf)
 
 data RequestType = GET | POST | PUT | DELETE | HEAD | OPTIONS | CONNECT | TRACE | PATCH deriving (Show, Eq)
 
@@ -18,18 +24,46 @@ data Request = Request {
 } deriving (Show, Eq)
 
 data Server = Server {
-    serverPort :: Int,
-    serverSocket :: Socket
+    host :: String,
+    port :: Int
 } deriving (Show, Eq)
+
+makeHeaders :: [(String, String)] -> String
+makeHeaders [] = ""
+makeHeaders ((key, value):xs) = key ++ ": " ++ value ++ "\r\n" ++ makeHeaders xs
 
 makeRequest :: Request -> String
 makeRequest request = 
-    let
-        requestType = show $ CutHttp.requestType request
-        requestPath = CutHttp.requestPath request
-        requestVersion = CutHttp.requestVersion request
-        requestHeaders = CutHttp.requestHeaders request
-        requestBody = CutHttp.requestBody request
-        stopAt = CutHttp.stopAt request
-    in
-        requestType ++ " " ++ requestPath ++ " " ++ requestVersion ++ "\r\n" ++ (makeHeaders requestHeaders) ++ "\r\n" ++ requestBody ++ stopAt
+    show (requestType request) ++ " " ++ requestPath request ++ " " ++ requestVersion request ++ "\r\n" ++
+    makeHeaders (requestHeaders request) ++
+    "\r\n" ++
+    requestBody request
+
+recvUntil :: Request -> Server -> IO String
+recvUntil request server = do
+    addrInfo <- getAddrInfo Nothing (Just (host server)) (Just (show (port server)))
+    let serverAddr = head addrInfo
+
+    socket <- socket (addrFamily serverAddr) Stream defaultProtocol
+    connect socket (addrAddress serverAddr)
+
+    let requestStr = makeRequest request
+    send socket (BS.pack requestStr)
+
+    response <- receiveInBatches socket (stopAt request)
+    close socket
+    return response
+
+receiveInBatches :: Socket -> String -> IO String
+receiveInBatches socket stopAt = receiveInBatches' socket stopAt ""
+
+receiveInBatches' :: Socket -> String -> String -> IO String
+receiveInBatches' socket stopAt accumulatedData = do
+    response <- recv socket 1
+    let responseStr = unpack response
+
+    if BS.null response
+        then return accumulatedData
+        else if stopAt `isInfixOf` (accumulatedData ++ responseStr)
+            then return (accumulatedData ++ responseStr)
+            else receiveInBatches' socket stopAt (accumulatedData ++ responseStr)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,22 @@
 import Test.Hspec
-import CutHttp(makeRequest, Request(..), RequestType(..), Server(..))
+import CutHttp(makeRequest, recvUntil, Request(..), RequestType(..), Server(..))
+import Network.Socket
 
 main :: IO ()
 main = hspec $ do
-  describe "make request" $ do
-    it "should return valid request" $ do
-        let server = Server 8080 undefined
-        server `shouldBe` Server 8080 undefined        
+    describe "make server" $ do
+        it "should return valid request" $ do
+            let server = Server "localhost" 8080
+            host server `shouldBe` "localhost"
+            port server `shouldBe` 8080
+
+    describe "make request string" $ do
+        it "should parse request correctly" $ do
+            let request = Request GET "/" "HTTP/1.1" [("Content-Type", "application/json")] "Hello" ""
+            makeRequest request `shouldBe` "GET / HTTP/1.1\r\nContent-Type: application/json\r\n\r\nHello"
+
+    describe "make socket" $ do
+        it "should return valid socket" $ do
+            let server = Server "194.135.85.93" 80
+            let request = Request GET "/" "HTTP/1.1" [("Content-Type", "application/json")] "Hello" "Bad"
+            recvUntil request server `shouldReturn` "HTTP/1.1 400 Bad"
